bb6c23cebf16c6991e84631ddc6f3a141ef15a93
ChanduGouda15 <chandugouda15@gmail.com>
2024-06-24 11:43:34 +0530

    modified js code

--- .vscode/Visual studio codes/Web Development/DOM/Dicee-Challenge/index.js
+++ .vscode/Visual studio codes/Web Development/DOM/Dicee-Challenge/index.js
@@ -6,7 +6,7 @@ var RandomNumber2 = Math.floor(Math.random()*6)+1;
 var RandomImage2 = "images/dice"+RandomNumber2+".png";
 document.querySelector(".img2").setAttribute("src",RandomImage2);
 
-if(RandomNumber1>RandomImage2)
+if(RandomNumber1>RandomNumber2)
     {
         document.querySelector("h1").innerText="Player 1 Wins";
     }
