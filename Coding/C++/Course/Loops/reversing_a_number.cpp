406a2388484b8b126ff748c63c97e6234db70436
ChanduGouda15 <ChanduGouda15@gmail.com>
2024-07-20 10:53:57 +0530

    modified the code

--- Coding/C++/Course/Loops/reversing_a_number.cpp
+++ Coding/C++/Course/Loops/reversing_a_number.cpp
@@ -6,13 +6,13 @@ int main() {
     int n;
     cin>>n;
     int temp = n;
- 
+    int rev=0;
     while(n>0)
     {
         int digits= n%10;
         n=n/10;
-        cout << digits;
+        rev = rev*10+digits;
     }
-
+    cout<<rev<<endl;
     return 0;
 }
