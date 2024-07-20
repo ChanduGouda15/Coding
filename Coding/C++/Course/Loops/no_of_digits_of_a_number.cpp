ab7209598cdcc62fa642551b657d0948aea29b30
ChanduGouda15 <ChanduGouda15@gmail.com>
2024-07-20 10:00:37 +0530

    no of digits in  a number

---
+++ Coding/C++/Course/Loops/no_of_digits_of_a_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int n;
+    cin>>n;
+    while(n>0)
+    {
+        int digits= n%10;
+        n=n/10;
+        cout << digits <<endl;
+    }
+
+    return 0;
+}
